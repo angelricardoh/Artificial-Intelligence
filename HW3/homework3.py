de16cb3a8bd2e83a9ec6ccf729e01ee2ec830858
Angel Ricardo Nieto Garcia <angelricardoh@gmail.com>
2019-11-17 19:19:09 -0800

    Renaming and refactor

--- HW3/homework3.py
+++ HW3/homework3.py
@@ -2,39 +2,51 @@ import os.path
 from os import path
 import collections
 import collections.abc
-import itertools
 
-class Expr:
+
+class ComplexSentence:
     def __init__(self, op, *args):
         self.op = str(op)
         self.args = args
 
     def __invert__(self):
-        return Expr('~', self)
+        return ComplexSentence('~', self)
 
     def __and__(self, rhs):
-        return Expr('&', self, rhs)
+        return ComplexSentence('&', self, rhs)
 
     def __or__(self, rhs):
-        if isinstance(rhs, Expression):
-            return Expr('|', self, rhs)
+        if isinstance(rhs, ComplexSentence):
+            return ComplexSentence('|', self, rhs)
         else:
-            return PartialExpr(rhs, self)
+            return Sentence(rhs, self)
 
     def __call__(self, *args):
         if self.args:
             raise ValueError('can only do a call for a Symbol, not an Expr')
         else:
-            return Expr(self.op, *args)
+            return ComplexSentence(self.op, *args)
 
     def __eq__(self, other):
-        return isinstance(other, Expr) and self.op == other.op and self.args == other.args
+        return isinstance(other, ComplexSentence) and self.op == other.op and self.args == other.args
 
     def __hash__(self):
         return hash(self.op) ^ hash(self.args)
 
+    def __repr__(self):
+        op = self.op
+        args = [str(arg) for arg in self.args]
+        if op.isidentifier():  # f(x) or f(x, y)
+            return '{}({})'.format(op, ', '.join(args)) if args else op
+        elif len(args) == 1:  # -x or -(x + 1)
+            return op + args[0]
+        else:  # (x - y)
+            opp = (' ' + op + ' ')
+            return '(' + opp.join(args) + ')'
+
+
 def Symbol(name):
-    return Expr(name)
+    return ComplexSentence(name)
 
 
 def symbols(names):
@@ -43,43 +55,47 @@ def symbols(names):
 
 def subexpressions(x):
     yield x
-    if isinstance(x, Expr):
+    if isinstance(x, ComplexSentence):
         for arg in x.args:
             yield from subexpressions(arg)
 
 
 def arity(expression):
-    if isinstance(expression, Expr):
+    if isinstance(expression, ComplexSentence):
         return len(expression.args)
-    else:  # expression is a number
+    else:
         return 0
 
 
-class PartialExpr:
+class Sentence:
     def __init__(self, op, lhs):
         self.op, self.lhs = op, lhs
 
     def __or__(self, rhs):
-        return Expr(self.op, self.lhs, rhs)
+        return ComplexSentence(self.op, self.lhs, rhs)
 
     def __repr__(self):
-        return "PartialExpr('{}', {})".format(self.op, self.lhs)
+        return "Sentence('{}', {})".format(self.op, self.lhs)
 
 
 def expr(x):
-    return eval(expr_handle_infix_ops(x), defaultkeydict(Symbol)) if isinstance(x, str) else x
+    return eval(expr_handle_infix_ops(x), DefaultKeyDict(Symbol)) if isinstance(x, str) else x
+
 
 def first(iterable, default=None):
     return next(iter(iterable), default)
+
 
 def extend(self, items):
     for item in items:
         self.append(item)
 
+
 def extend(s, var, val):
     s2 = s.copy()
     s2[var] = val
     return s2
+
 
 def remove_all(item, seq):
     if isinstance(seq, str):
@@ -91,11 +107,12 @@ def remove_all(item, seq):
     else:
         return [x for x in seq if x != item]
 
+
 def unique(seq):
     return list(set(seq))
 
 
-def issequence(x):
+def is_sequence(x):
     return isinstance(x, collections.abc.Sequence)
 
 
@@ -105,7 +122,7 @@ def expr_handle_infix_ops(x):
     return x
 
 
-class defaultkeydict(collections.defaultdict):
+class DefaultKeyDict(collections.defaultdict):
     def __missing__(self, key):
         self[key] = result = self.default_factory(key)
         return result
@@ -117,10 +134,6 @@ def is_symbol(s):
 
 def is_var_symbol(s):
     return is_symbol(s) and s[0].islower()
-
-
-def is_prop_symbol(s):
-    return is_symbol(s) and s[0].isupper()
 
 
 def variables(s):
@@ -153,7 +166,7 @@ def eliminate_implications(s):
         return (a & ~b) | (~a & b)
     else:
         assert s.op in ('&', '|', '~')
-        return Expr(s.op, *args)
+        return ComplexSentence(s.op, *args)
 
 
 def move_not_inwards(s):
@@ -173,7 +186,7 @@ def move_not_inwards(s):
     elif is_symbol(s.op) or not s.args:
         return s
     else:
-        return Expr(s.op, *list(map(move_not_inwards, s.args)))
+        return ComplexSentence(s.op, *list(map(move_not_inwards, s.args)))
 
 
 def distribute_and_over_or(s):
@@ -206,7 +219,7 @@ def associate(op, args):
     elif len(args) == 1:
         return args[0]
     else:
-        return Expr(op, *args)
+        return ComplexSentence(op, *args)
 
 
 def dissociate(op, args):
@@ -223,11 +236,11 @@ def dissociate(op, args):
     return result
 
 
-def conjuncts(s):
+def enum_conjunctions(s):
     return dissociate('&', [s])
 
 
-def disjuncts(s):
+def enum_disjunctions(s):
     return dissociate('|', [s])
 
 
@@ -240,11 +253,11 @@ def unify(x, y, s={}):
         return unify_var(x, y, s)
     elif is_variable(y):
         return unify_var(y, x, s)
-    elif isinstance(x, Expr) and isinstance(y, Expr):
+    elif isinstance(x, ComplexSentence) and isinstance(y, ComplexSentence):
         return unify(x.args, y.args, unify(x.op, y.op, s))
     elif isinstance(x, str) or isinstance(y, str):
         return None
-    elif issequence(x) and issequence(y) and len(x) == len(y):
+    elif is_sequence(x) and is_sequence(y) and len(x) == len(y):
         if not x:
             return s
         return unify(x[1:], y[1:], unify(x[0], y[0], s))
@@ -253,7 +266,7 @@ def unify(x, y, s={}):
 
 
 def is_variable(x):
-    return isinstance(x, Expr) and not x.args and x.op[0].islower()
+    return isinstance(x, ComplexSentence) and not x.args and x.op[0].islower()
 
 
 def unify_var(var, x, s):
@@ -274,7 +287,7 @@ def occur_check(var, x, s):
         return True
     elif is_variable(x) and x in s:
         return occur_check(var, s[x], s)
-    elif isinstance(x, Expr):
+    elif isinstance(x, ComplexSentence):
         return (occur_check(var, x.op, s) or
                 occur_check(var, x.args, s))
     elif isinstance(x, (list, tuple)):
@@ -288,35 +301,36 @@ def subst(s, x):
         return [subst(s, xi) for xi in x]
     elif isinstance(x, tuple):
         return tuple([subst(s, xi) for xi in x])
-    elif not isinstance(x, Expr):
+    elif not isinstance(x, ComplexSentence):
         return x
     elif is_var_symbol(x.op):
         return s.get(x, x)
     else:
-        return Expr(x.op, *[subst(s, arg) for arg in x.args])
+        return ComplexSentence(x.op, *[subst(s, arg) for arg in x.args])
 
 
 def cascade_substitution(s):
     for x in s:
         s[x] = subst(s, s.get(x))
-        if isinstance(s.get(x), Expr) and not is_variable(s.get(x)):
+        if isinstance(s.get(x), ComplexSentence) and not is_variable(s.get(x)):
             s[x] = subst(s, s.get(x))
 
 
 def standardize_variables(sentence, dic=None):
     if dic is None:
         dic = {}
-    if not isinstance(sentence, Expr):
+    if not isinstance(sentence, ComplexSentence):
         return sentence
     elif is_var_symbol(sentence.op):
         if sentence in dic:
             return dic[sentence]
         else:
-            v = Expr('v_{}'.format(next(standardize_variables.counter)))
+            standardize_variables.counter += 1
+            v = ComplexSentence('v_' + str(standardize_variables.counter))
             dic[sentence] = v
             return v
     else:
-        return Expr(sentence.op, *[standardize_variables(a, dic) for a in sentence.args])
+        return ComplexSentence(sentence.op, *[standardize_variables(a, dic) for a in sentence.args])
 
 
 class KB():
@@ -332,19 +346,13 @@ class KB():
     def ask_if_true(self, query):
         return pl_resolution(self, query)
 
-    def retract(self, sentence):
-        self.clauses.remove(sentence)
-
-    def fetch_rules_for_goal(self, goal):
-        return self.clauses
-
 
 def pl_resolution(kb, alpha):
     cnf_clauses = []
     for clause in kb.clauses:
         cnf_clauses.append(to_cnf(clause))
         # print(to_cnf(clause))
-    clauses = cnf_clauses + conjuncts(to_cnf(~alpha))
+    clauses = cnf_clauses + enum_conjunctions(to_cnf(~alpha))
     new = set()
     while True:
         n = len(clauses)
@@ -366,24 +374,23 @@ def pl_resolve(ci, cj):
     clauses = []
     # print("ci = ", ci)
     # print("cj = ", cj)
-    for di in disjuncts(ci):
-        for dj in disjuncts(cj):
+    for di in enum_disjunctions(ci):
+        for dj in enum_disjunctions(cj):
+            cnf_dj_inverse = to_cnf(~dj)
             # print("di = ", di)
-            # print("dj = ", to_cnf(~dj))
-            phi = unify(di, to_cnf(~dj))
-            if phi or di == to_cnf(~dj):
-                new_clause = associate('|', unique(remove_all(di, disjuncts(ci))) + remove_all(dj, disjuncts(cj)))
+            # print("~dj = ", cnf_dj_inverse)
+            phi = unify(di, cnf_dj_inverse)
+            if phi or di == cnf_dj_inverse:
+                new_clause = associate('|', unique(remove_all(di, enum_disjunctions(ci))) + remove_all(dj, enum_disjunctions(cj)))
                 norm_clause = subst(phi, new_clause)
                 clauses.append(norm_clause)
     return clauses
 
 
 # Global variables
-Number = (int, float, complex)
-Expression = (Expr, Number)
 infix_ops = '=>'.split()
-_op_identity = {'&': True, '|': False, '+': 0, '*': 1}
-standardize_variables.counter = itertools.count()
+_op_identity = {'&': True, '|': False}
+standardize_variables.counter = 0
 
 
 def main():
